8df286f7e2a4cd95bc265390aeed439cdff900e5
Asmaa Belhadj <abelhadj@studenti.uninsubria.it>
2023-01-20 17:19:37 +0100

    Added service interfaces

---
+++ PizzaRestaurantService/src/main/java/com/myrestaurant/store/pizzarestaurantservice/Service/PizzaService.java
@@ -0,0 +1,6 @@
+package com.myrestaurant.store.pizzarestaurantservice.Service;
+
+import com.myrestaurant.store.pizzarestaurantservice.Model.Pizza;
+
+public interface PizzaService extends GenericService<Pizza,Long> {
+}
---
+++ PizzaRestaurantService/src/main/java/com/myrestaurant/store/pizzarestaurantservice/Service/ToppingService.java
@@ -0,0 +1,6 @@
+package com.myrestaurant.store.pizzarestaurantservice.Service;
+
+import com.myrestaurant.store.pizzarestaurantservice.Model.Topping;
+
+public interface ToppingService extends GenericService<Topping,Long>{
+}
