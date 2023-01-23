1a4ed09fd30c33f7ce2cedc572ca89b08a7d36b6
Asmaa Belhadj <abelhadj@studenti.uninsubria.it>
2023-01-23 09:43:24 +0100

    Added Controller interfaces

---
+++ PizzaRestaurantService/src/main/java/com/myrestaurant/store/pizzarestaurantservice/Controller/PizzaController.java
@@ -0,0 +1,29 @@
+package com.myrestaurant.store.pizzarestaurantservice.Controller;
+
+import com.myrestaurant.store.pizzarestaurantservice.DTO.PizzaDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Api(tags = "Pizza Api")
+public interface PizzaController {
+
+    @ApiOperation("Add new pizza")
+    PizzaDTO save(@RequestBody PizzaDTO pizzaDTO);
+
+    @ApiOperation("Find pizza by Id")
+    PizzaDTO findById(@PathVariable("id") Long id);
+
+    @ApiOperation("Delete by Id")
+    void delete(@PathVariable("id") Long id);
+
+    @ApiOperation("Find all pizzas ")
+    List<PizzaDTO> list();
+
+    @ApiOperation("Update pizza")
+    PizzaDTO update(@RequestBody PizzaDTO pizzaDTO, @PathVariable("id") Long id);
+
+}
