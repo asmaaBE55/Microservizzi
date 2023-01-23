ce37ec8b3b3641ebfecbe24216e08078c2f4a603
Asmaa Belhadj <abelhadj@studenti.uninsubria.it>
2023-01-23 10:38:56 +0100

    Added Controller implementations

---
+++ PizzaRestaurantService/src/main/java/com/myrestaurant/store/pizzarestaurantservice/Controller/Impl/PizzaControllerImpl.java
@@ -0,0 +1,64 @@
+package com.myrestaurant.store.pizzarestaurantservice.Controller.Impl;
+
+import com.myrestaurant.store.pizzarestaurantservice.Controller.PizzaController;
+import com.myrestaurant.store.pizzarestaurantservice.DTO.PizzaDTO;
+import com.myrestaurant.store.pizzarestaurantservice.Mapper.PizzaMapper;
+import com.myrestaurant.store.pizzarestaurantservice.Model.Pizza;
+import com.myrestaurant.store.pizzarestaurantservice.Service.PizzaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pizzas")
+@RequiredArgsConstructor
+public class PizzaControllerImpl implements PizzaController {
+    private final PizzaService pizzaService;
+    private final PizzaMapper pizzaMapper;
+
+    @Override
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public PizzaDTO save(@RequestBody PizzaDTO pizzaDTO) {
+        Pizza pizza = pizzaMapper.asEntity(pizzaDTO);
+        return pizzaMapper.asDTO(pizzaService.save(pizza));
+    }
+    /**
+     * STEPS DETTAGLIATE DEL METODO POST
+     * Pizza pizza = pizzaMapper.asEntity(pizzaDTO);
+     * Pizza saved = pizzaService.save(pizza);
+     * PizzaDTO dto = pizzaMapper.asDTO(pizzaService.save(saved));
+     * return dto;
+     **/
+    @Override
+    @GetMapping("/{id}")
+    public PizzaDTO findById(@PathVariable Long id) {
+        Pizza pizza=pizzaService.findById(id).orElse(null);
+        return pizzaMapper.asDTO(pizza);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+     pizzaService.deleteById(id);
+    }
+
+    @Override
+    @GetMapping
+    public List<PizzaDTO> list() {
+        return pizzaMapper.asDTOlist(pizzaService.findAll());
+    }
+    /** STEPS DETTAGLIATE DEL METODO GET LIST
+     * List<Pizza> pizzas = pizzaService.findAll();
+     * return pizzaMapper.asDTOList(pizzas);
+     * **/
+
+    @Override
+    @PutMapping("/{id}")
+    public PizzaDTO update(@RequestBody PizzaDTO pizzaDTO, @PathVariable("id") Long id) {
+      Pizza pizza=pizzaMapper.asEntity(pizzaDTO);
+      return pizzaMapper.asDTO(pizzaService.update(pizza,id));
+    }
+}
---
+++ PizzaRestaurantService/src/main/java/com/myrestaurant/store/pizzarestaurantservice/Controller/Impl/ToppingControllerImpl.java
@@ -0,0 +1,52 @@
+package com.myrestaurant.store.pizzarestaurantservice.Controller.Impl;
+
+import com.myrestaurant.store.pizzarestaurantservice.Controller.ToppingController;
+import com.myrestaurant.store.pizzarestaurantservice.DTO.ToppingDTO;
+import com.myrestaurant.store.pizzarestaurantservice.Mapper.ToppingMapper;
+import com.myrestaurant.store.pizzarestaurantservice.Model.Topping;
+import com.myrestaurant.store.pizzarestaurantservice.Service.ToppingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/toppings")
+@RequiredArgsConstructor
+public class ToppingControllerImpl implements ToppingController {
+    private final ToppingService toppingService;
+    private final ToppingMapper toppingMapper;
+
+    @Override
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ToppingDTO save(ToppingDTO toppingDTO) {
+        Topping topping = toppingMapper.asEntity(toppingDTO);
+        return toppingMapper.asDTO(toppingService.save(topping));
+    }
+
+    @Override
+    @GetMapping("/{id}")
+    public ToppingDTO findById(Long id) {
+        Topping topping=toppingService.findById(id).orElse(null);
+        return toppingMapper.asDTO(topping);
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+     toppingService.deleteById(id);
+    }
+
+    @Override
+    public List<ToppingDTO> list() {
+        return toppingMapper.asDTOlist(toppingService.findAll());
+    }
+
+    @Override
+    public ToppingDTO update(ToppingDTO toppingDTO, Long id) {
+      Topping topping=toppingMapper.asEntity(toppingDTO);
+      return toppingMapper.asDTO(toppingService.update(topping,id));
+    }
+}
